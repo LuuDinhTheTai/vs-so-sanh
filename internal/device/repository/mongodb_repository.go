49f928bf39ccac35167935e8d2d529280a747f62
Luu Dinh The Tai <luudinthetai741@gmail.com>
2025-12-17 12:53:29 +0700

    docs: README.md

--- internal/device/repository/mongodb_repository.go
+++ internal/device/repository/mongodb_repository.go
@@ -36,17 +36,12 @@ func (m *MongoDbDeviceRepository) FindByName(c context.Context, deviceName strin
 	collection := m.client.Database(m.cfg.Database.DBName).Collection("brands")
 
 	pipeline := mongo.Pipeline{
-		// 1. Tìm document Brand chứa điện thoại này (để lọc bớt dữ liệu rác)
 		{{"$match", bson.M{"devices.model_name": deviceName}}},
 
-		// 2. Tách mảng devices ra thành các document riêng lẻ
 		{{"$unwind", "$devices"}},
 
-		// 3. Lọc lại một lần nữa để lấy đúng điện thoại cần tìm
 		{{"$match", bson.M{"devices.model_name": deviceName}}},
 
-		// 4. (Quan trọng) Đưa object device lên làm root document
-		// Lúc này kết quả trả về sẽ khớp trực tiếp với struct model.Device
 		{{"$replaceRoot", bson.M{"newRoot": "$devices"}}},
 	}
 
@@ -56,17 +51,15 @@ func (m *MongoDbDeviceRepository) FindByName(c context.Context, deviceName strin
 	}
 	defer cursor.Close(ctx)
 
-	// Kiểm tra xem có kết quả không
 	if cursor.Next(ctx) {
 		var device model.Device
-		// Decode trực tiếp vào model.Device, không cần qua struct trung gian
 		if err := cursor.Decode(&device); err != nil {
 			return nil, err
 		}
 		return &device, nil
 	}
 
-	return nil, nil // Không tìm thấy
+	return nil, nil
 }
 
 func (m *MongoDbDeviceRepository) FindTop20(c context.Context) ([]model.Device, error) {
@@ -76,17 +69,14 @@ func (m *MongoDbDeviceRepository) FindTop20(c context.Context) ([]model.Device, 
 	collection := m.client.Database(m.cfg.Database.DBName).Collection("brands")
 
 	pipeline := mongo.Pipeline{
-		// 1. Tách mảng devices ra thành từng dòng riêng biệt
 		{{"$unwind", "$devices"}},
 
-		// 2. Bỏ qua trường specifications (Project)
 		{{"$project", bson.D{
 			{"model_name", "$devices.model_name"},
 			{"imageUrl", "$devices.imageUrl"},
 			{"_id", 0}, // Không lấy ID của Brand
 		}}},
 
-		// 3. Giới hạn 20 cái đầu tiên
 		{{"$limit", 20}},
 	}
 
